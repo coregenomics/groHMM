6ebfe54b95ce2c7426fe2eac45250bfb434fb125
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-01-24 19:31:23 -0500

    TST: Separate break and combine functions

--- tests/testthat/test_gene_boundary_corrections.R
+++ tests/testthat/test_gene_boundary_corrections.R
@@ -5,7 +5,10 @@ context("Gene boundary corrections")
 tx <- GRanges(c("chr7:1000-30000"), strand="+")
 annox <- GRanges(c("chr7:1000-11000",
                    "chr7:20000-30000"), strand="+")
-numTx <- function(annox) length(ranges(breakTranscriptsOnGenes(tx, annox)))
+txC <- annox
+annoxC <- tx
+numTxBr <- function(annox) length(ranges(breakTranscriptsOnGenes(tx, annox)))
+numTxCb <- function(txC) length(ranges(combineTranscripts(txC, annoxC)))
 
 test_that("Inputs 'tx' and 'annox' are validated", {
     expect_error(breakTranscriptsOnGenes(NULL, annox),
@@ -35,16 +38,20 @@ test_that("breakTranscriptsOnGenes returns 'gap' sized IRanges", {
                         end=-gap - 1))
 })
 
-test_that("breakTranscriptsOnGenes honors 'geneSize'", {
+test_that("Gene repair functions honor 'geneSize'", {
     geneSize <- formals(breakTranscriptsOnGenes)$geneSize
     annoxShortest <- resize(annox, geneSize + 1)
     annoxTooShort <- narrow(annoxShortest, end=-2)
 
-    ## Minimum acceptable annotation geneSize should work just as well
-    ## as a longer annotation.
-    expect_equal(numTx(annoxShortest), 2)
-    ## Annotation should be ignored when geneSize is too short.
-    expect_equal(numTx(annoxTooShort), 1)
+    expect_equal(numTxBr(annoxShortest), 2)
+    expect_equal(numTxBr(annoxTooShort), 1)
+
+    geneSize <- formals(combineTranscripts)$geneSize
+    txShortest <- resize(txC, geneSize + 1)
+    txTooShort <- narrow(txShortest, end=-2)
+
+    expect_equal(numTxCb(txShortest), 2)
+    expect_equal(numTxCb(txTooShort), 1)
 })
 
 test_that("breakTranscriptsOnGenes honors 'threshold'", {
@@ -52,7 +59,7 @@ test_that("breakTranscriptsOnGenes honors 'threshold'", {
     annoxMinOverlap <- shift(annox, width(annox) * (1 - threshold))
     annoxInsuffOverlap <- shift(annox, width(annox) * (1.05 - threshold))
 
-    expect_equal(numTx(annoxMinOverlap), 2)
+    expect_equal(numTxBr(annoxMinOverlap), 2)
     ## The second gene now does not overlap with the transcript.
-    expect_equal(numTx(annoxInsuffOverlap), 1)
+    expect_equal(numTxBr(annoxInsuffOverlap), 1)
 })
