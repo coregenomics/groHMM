7a54292396e874dd3802c7fc0f3059d42e638f30
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-08-02 14:53:03 -0400

    TST: Use explicit functions after moving packages to imports

--- tests/testthat/helper_fixtures.R
+++ tests/testthat/helper_fixtures.R
@@ -1,17 +1,16 @@
 library(groHMM)
 
-tx <- GRanges(c("chr7:1000-30000"), strand="+")
-annox <- GRanges(c("chr7:1000-11000",
-                   "chr7:20000-30000"), strand="+")
+tx <- GenomicRanges::GRanges(c("chr7:1000-30000"), strand="+")
+annox <- GenomicRanges::GRanges(c("chr7:1000-11000",
+                                  "chr7:20000-30000"), strand="+")
 
 ## Fixtures (identical to example in polymeraseWave man page)
-genes <- GRanges("chr7", IRanges(2394474,2420377), strand="+",
-                 SYMBOL="CYP2W1", ID="54905")
-non_map <- GRanges("chr7", IRanges(2394474,2420377), strand="+")
+genes <- GenomicRanges::GRanges("chr7", IRanges::IRanges(2394474,2420377),
+                                strand="+", SYMBOL="CYP2W1", ID="54905")
 read_bams <- function(files) {
     lapply(files, function(x) {
-        as(readGAlignments(system.file("extdata", x, package="groHMM")),
-           "GRanges")
-        })
+        file_ <- system.file("extdata", x, package="groHMM", lib.loc=.Library)
+        as(GenomicAlignments::readGAlignments(file_), "GRanges")
+    })
 }
 reads <- read_bams(c("S0mR1.bam", "S40mR1.bam"))
