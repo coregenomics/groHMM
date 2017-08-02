27e4d0810c0218861789b77cfd32eb5ad6a174ef
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-08-02 14:54:30 -0400

    TST: Fixed polymeraseWave bug with data.frame, drop NonMap argument

--- tests/testthat/test_polymeraseWave.R
+++ tests/testthat/test_polymeraseWave.R
@@ -9,17 +9,13 @@ expected <- data.frame(
   Rate = 24400,
   minOfMax = NA,
   minOfAvg = NA,
-  ID = "CYP2W1",
-  ExternalID = factor(54905)
+  SYMBOL = "CYP2W1",
+  ID = "54905",
+  stringsAsFactors = FALSE
 )
 
 test_that("polymeraseWave returns expected data.frame", {
   pw <- polymeraseWave(reads[[1]], reads[[2]], genes, approxDist)
   expect_s3_class(pw, "data.frame")
   expect_equal(pw, expected, tolerance = 1e-7)
-})
-
-test_that("countMappableReadsInInterval validates NonMap", {
-  expect_error(polymeraseWave(reads[[1]], reads[[2]], genes,
-                        approxDist, NonMap=TRUE), "NonMap")
 })
