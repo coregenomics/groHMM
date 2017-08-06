acf0b4ea9c8964ed5bf347afbcc3b2679c7a8706
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-08-05 20:34:44 -0400

    TST: Read in bam files as .rda for reliable unit tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,8 @@
+## Workaround for system.file() failing with devtools::check() and Travis CI:
+## https://github.com/hadley/testthat/issues/86#issuecomment-143878211
+Sys.setenv("R_TESTS" = "")
+
 library(testthat)
 library(groHMM)
 
-test_package("groHMM")
+test_check("groHMM")
---
+++ tests/testthat/helper_fixtures.R
@@ -0,0 +1,13 @@
+library(groHMM)
+library(GenomicAlignments)
+library(GenomicRanges)
+
+tx <- GRanges(c("chr7:1000-30000"), strand="+")
+annox <- GRanges(c("chr7:1000-11000",
+                   "chr7:20000-30000"), strand="+")
+
+## Fixtures (identical to example in polymeraseWave man page)
+genes <- GRanges("chr7", IRanges(2394474,2420377),
+                 strand="+", SYMBOL="CYP2W1", ID="54905")
+load("sr1.rda")
+reads <- sr1
