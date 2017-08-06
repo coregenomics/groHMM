3bd967684dc28632eaaed7b56848c2d0f859de57
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-08-06 12:04:33 -0400

    MAINT: Drop readBed() in leiu of rtracklayer::import.bed()

--- R/limitToXkb.R
+++ R/limitToXkb.R
@@ -62,34 +62,3 @@ limitToXkb <- function(features, offset=1000, size=13000) {
 
     return(features)
 }
-
-#' readBed Returns a GenomicRanges object constructed from the specified bed
-#' file.
-#'
-#' Bed file format is assumed to be either four column: seqnames, start, end, 
-#' strand columns; or six column: seqnames, start, end, name, score, and strand.
-#' Three column format is also possible when there is no strand information.
-#'
-#' Any additional arguments available to read.table can be specified.
-#'
-#' @param file Path to the input file.
-#' @param ... Extra argument passed to read.table
-#' @return Returns GRanges object representing mapped reads.
-#' @author Minho Chae and Charles G. Danko.
-readBed <- function(file, ...) {
-    df <- read.table(file, ...)
-    if(NCOL(df) == 3) {
-        colnames(df) <-
-            c("seqnames", "start", "end")
-        df <- cbind(df, strand=Rle("*", NROW(df)))
-    }
-    if(NCOL(df) == 4)
-        colnames(df) <-
-            c("seqnames", "start", "end", "strand")
-    if(NCOL(df) == 6)
-        colnames(df) <-
-            c("seqnames", "start", "end", "name", "score", "strand")
-    return(GRanges(
-        seqnames=Rle(df$seqnames), ranges=IRanges(df$start, df$end),
-        strand=Rle(strand(df$strand))))
-}
