3d6f8c234ca19fc28ad5b0f37338a304e7a8c188
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-08-23 20:56:35 -0400

    MAINT: Use compact GRanges notation

--- tests/testthat/helper_fixtures.R
+++ tests/testthat/helper_fixtures.R
@@ -7,7 +7,6 @@ annox <- GRanges(
     GENE_ID=c("itchy", "scratchy"))
 
 ## Fixtures (identical to example in polymeraseWave man page)
-genes <- GRanges(
-    "chr7", IRanges(2394474, 2420377), strand="+", SYMBOL="CYP2W1", ID="54905")
+genes <- GRanges("chr7:2394474-2420377:+", SYMBOL="CYP2W1", ID="54905")
 load("sr1.rda")
 reads <- sr1
